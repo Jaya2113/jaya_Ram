e2f1aea83dcb045067cd53b40c6268708d68283a
Jaya2113 <105451931+Jaya2113@users.noreply.github.com>
2022-05-24 12:17:27 +0530

    Coding challenge
    commit 3

--- git/FirstJavaProject/src/Customer.java
+++ git/FirstJavaProject/src/Customer.java
@@ -1,29 +1,36 @@
-
-/* Question
-
-Write an algorithm to create a class Customer that contains 
-
-          fullName(String) and age(int) as variables in class. 
-
-Create a main method, assign values to the variables in the main method and print the values of variables using the object of the main method. 
-
-After writing the algorithm, write suitable code, run the program, and check the output.*/
-
+/* Write an algorithm to implement access modifier functionality.
+ * Create two separate classes Customer & CustomerDriver inside com.greatlearning.javacore; package.
+ * Create various variables in class Customer of different access modifiers such as (private, public,protected) and initialize them with some values.
+ * Algorithm:
+ * 1. Start
+ * 2. Create a package co.greatlearning.javacore;
+ * 3. Create Customer and CustomerDriver class under same package.
+ * 4. Declare int age as private, float weight as protected and float height as public data members in class Customer.
+ * 5.create a main method in CustomerDriver class.
+ * 6. create object obj in class CustomerDriver to access the members from class Customer.
+ * 7. Display all the variables in class CustomerDriver.
  
-class Customer {
-	int age;
-	String fullName;
-}
-class custo
-{
-	public static void main(String[] args) 
-	{
-		Customer s1=new Customer();
-		s1.age=40;
-		s1.fullName="Krishna Shree Ram";
-		System.out.println(s1.age+ " " +s1.fullName);
-		
+ */
 
+package com.greatlearning.corejava;
+
+public class Customer {
+	//private int age=20;
+	protected float weight=35;
+	public float height=5;
+}
+	
+	 class CustomerDriver
+	{
+		
+	public static void main(String[] args) {
+		
+	Customer obj=new Customer();
+	//System.out.println("The age is:"+ obj.age);
+	System.out.println("The weight is:"+ obj.weight);
+	System.out.println("The height is:" + obj.height);
+	
+	}
 	}
 
-}
+
